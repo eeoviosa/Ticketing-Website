d8932acd138d934c7a65c1a9ed92115c018ea0bc
eeoviosa <116686424+eeoviosa@users.noreply.github.com>
2023-03-17 20:39:55 -0500

    Created a ticket_form

--- Graduation/Graduation/urls.py
+++ Graduation/Graduation/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tickets', include('tickets.urls'))
+    path('tickets/', include('tickets.urls'))
 ]
--- Graduation/tickets/views.py
+++ Graduation/tickets/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 max_tickets = 6
 def add(request):
-    return render(request, 'first/ticket_form.html', {
-        "max_tickets": max_tickets
+    return render(request, 'tickets/ticket_form.html', {
+        "max_tickets": range(1, max_tickets + 1)
     })
 def view(request):
     pass
