e8fb307eb8d1f22e1b0c03099e55de45d5ec0e4e
eeoviosa <116686424+eeoviosa@users.noreply.github.com>
2023-03-17 20:14:04 -0500

    Made changes to layout file

--- Graduation/tickets/views.py
+++ Graduation/tickets/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+max_tickets = 6
 def add(request):
-    pass
+    return render(request, 'first/ticket_form.html', {
+        "max_tickets": max_tickets
+    })
 def view(request):
     pass
 def login(request):
