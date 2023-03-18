4c5a98a217195f5b3b3225849fb688e4140b17c6
eeoviosa <116686424+eeoviosa@users.noreply.github.com>
2023-03-18 01:54:51 -0500

    Finished after login functionalities

--- Graduation/tickets/models.py
+++ Graduation/tickets/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Users(models.Model):
     tickets_ordered = models.IntegerField()
     student_name = models.CharField(max_length=64)
-    studentID = models.IntegerField()
+    studentID = models.IntegerField(unique=True)
 
     def __str__(self):
         return f"Ticket_Ordered: {self.tickets_ordered}, Student_Name: {self.student_name}, StudentID: {self.studentID}"
--- Graduation/tickets/views.py
+++ Graduation/tickets/views.py
@@ -9,9 +9,10 @@ def add(request):
     if request.method == "POST":
         user = Users(student_name = request.POST["name"], studentID = request.POST["studentid"], tickets_ordered = request.POST["num_of_tickets"])
         user.save()
-        return render(request, 'tickets/registrants.html',
+        return render(request, 'tickets/confirm.html',
                                     {
-                                        "users": user
+                                        "users": Users.objects.all().values()
+                            
                                     })
     return render(request, 'tickets/ticket_form.html', {
         "max_tickets": range(1, max_tickets + 1)
