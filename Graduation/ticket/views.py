721d233e16e6021fd9e37c037ed6aca7ea79ecb2
eeoviosa <116686424+eeoviosa@users.noreply.github.com>
2023-03-21 16:57:37 -0500

    working draft

--- Graduation/ticket/views.py
+++ Graduation/ticket/views.py
@@ -4,16 +4,18 @@ from django.contrib.auth.models import User
 from .models import Ticket_Request
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.core.cache import cache
 
 # Create your views here.
-
-max_tickets = 6
+if cache.get("rem_tickets") == None:
+    cache.set("rem_tickets", 100)
+max_tickets = 4
 message = 'Invalid Credentials'
 
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
-    return render(request, 'tickets/home.html')
+    return render(request, 'tickets/home.html', {"available": cache.get("rem_tickets")})
     
 
 
@@ -25,18 +27,26 @@ def add(request):
         if Ticket_Request.objects.filter(studentID = request.POST['sid']):
             info = Ticket_Request.objects.get(studentID = request.POST["sid"])
             return render(request, 'tickets/registrants.html', {'info': info,
-                                                                'id' : request.POST["sid"]
-                                                            })
+                                                                "available": cache.get("rem_tickets")})
+        elif(int(request.POST["tickets_number"]) > cache.get("rem_tickets")):
+            return render(request, "tickets/ticket_form.html", {
+                "message": "Number of Tickets Ordered exceed the Number of Tickets Available, Adjust the Amount Ordered",
+                "available": cache.get("rem_tickets")
+            })
         users.save()
-        return render(request, 'tickets/confirm.html')
+        curr = cache.get("rem_tickets") - int(request.POST["tickets_number"])
+        cache.set("rem_tickets", curr)
+        return render(request, 'tickets/confirm.html', {"available": cache.get("rem_tickets")})
         
 
     return render(request, 'tickets/ticket_form.html', {
-        "max_tickets": range(1, max_tickets + 1)
+        "max_tickets": range(1, max_tickets + 1),
+        "available": cache.get("rem_tickets")
     })
+
 def logt(request):
     logout(request)
-    return render(request, 'tickets/login.html')
+    return render(request, 'tickets/login.html' )
 
    
 def logn(request):
@@ -51,11 +61,14 @@ def logn(request):
     return render(request, 'tickets/login.html')
    
 def newForm(request):
-    try:
+    try: 
+        new = cache.get("rem_tickets") + int(request.POST["ticket_order"])
+        cache.set("rem_tickets", new)
         user = Ticket_Request.objects.get(studentID = request.POST["id"])
         user.delete()
         return HttpResponseRedirect(reverse(add))
     except Exception:
-        return render(request, 'tickets/registrants.html', {'message': "Student ID Not in Database or Typed Incorrectly"})
+        return render(request, 'tickets/registrants.html', {'message': "Student ID Not in Database or Typed Incorrectly", 
+                                                            "available": cache.get("rem_tickets")})
 
 
